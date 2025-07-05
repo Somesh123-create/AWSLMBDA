d450d00fcec07af10f79712e328d4c74636160b0
somesh hiremath <someshhiremath9@yahoo.com>
2025-07-05 18:18:09 +0530

    adding multi processing

--- shared/mylib/utils.py
+++ shared/mylib/utils.py
@@ -1,2 +1,12 @@
 def my_function(x, output_queue):
     output_queue.put((x, x * 10))
+    
+def my_function_ml_procs(num, conn):
+    try:
+        # Do some computation
+        result = {"input": num, "output": num * 2}  # Example
+        conn.send(result)
+    except Exception as e:
+        conn.send({"error": str(e)})
+    finally:
+        conn.close()
